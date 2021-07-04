474f779cf1cdb779dddc981f0bb0f07567b3d961
David Jessup <jessup@adobe.com>
2021-07-04 10:58:25 +1000

    Further tweak bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1350,7 +1350,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 230
+#define X_BED_SIZE 225
 #define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
